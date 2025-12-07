f3c2756526dc1e3159e8ebc62ef96872598f5acd
developer-siddhisoftwarecompany <dev.siddhisoftwarecompany@gmail.com>
2025-12-07 20:24:24 +0530

    Cast user ID to integer in profile response

--- auth_get_profile.php
+++ auth_get_profile.php
@@ -2,16 +2,16 @@
 require 'config.php';
 require 'helpers.php';
 
-// Fetch user profile
 // Endpoint: /auth/profile
 // Method: GET
 
 requireMethod('GET');
 
+// requireAuth() MUST return a user array using PDO fetch
 $user = requireAuth($conn);
 
 sendResponse(true, 'Profile fetched', [
-    'id'    => $user['id'],
+    'id'    => (int) $user['id'],
     'name'  => $user['name'],
     'email' => $user['email']
 ]);
